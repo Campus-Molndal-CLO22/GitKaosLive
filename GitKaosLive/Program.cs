1214c42cf5e6eea74e1ac6788fd816ab8b797226
Marcus Medina <medina.jobb@outlook.com>
2022-10-11 11:30:17 +0200

    Snyggare kommentar

--- GitKaosLive/Program.cs
+++ GitKaosLive/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitKaosLive!");
 
-// Git kommandon
-// Init = Skapar ett repo lokalt
-// Add = Lägger till senaste ändringar i git databasen
-// Commit = lägger det i vår lokala repo
-// Push = Skicka från lokala repot till servern
+// -----------------------------------------------------
+// Init = Skapar ett repo lokalt                       -
+// -----------------------------------------------------
+
+// -----------------------------------------------------
+// Git kommandon                                       -
+// Används mycket                                      -
+// -----------------------------------------------------
+// Add = Lägger till senaste ändringar i git databasen -
+// Commit = lägger det i vår lokala repo               -
+// Push = Skicka från lokala repot till servern        -
+// Pull = Hämtar från servern till det lokala repot    -
+// -----------------------------------------------------
