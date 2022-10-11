9a7b707d2b586685705f2c864287c9918a416ffd
Marcus Medina <medina.jobb@outlook.com>
2022-10-11 11:16:23 +0200

    la till kommentarer

--- GitKaosLive/Program.cs
+++ GitKaosLive/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitKaosLive!");
 
-
+// Git kommandon
+// Init = Skapar ett repo lokalt
+// Add = Lägger till senaste ändringar i git databasen
+// 
